da4f94b80e51d68381d259585b72d94fb2764dfb
panu0 <panu.linnal4@gmail.com>
2018-08-31 22:21:41 +0300

    Clean up boilerplate

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Navbar = () => (
+  <nav className="navbar is-transparent">
+    <div className="container">
+      <div className="navbar-start">
+        <Link className="navbar-item" exact to="/">
+          ETUSIVU
+        </Link>
+        <Link className="navbar-item" to="/about">
+          INFO
+        </Link>
+        <Link className="navbar-item" to="/event">
+          TAPAHTUMAT
+        </Link>
+      </div>
+      <div className="navbar-end" />
+    </div>
+  </nav>
+)
+
+export default Navbar
