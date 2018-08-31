01b7366a3e1c33656d5392248f9b31e2b0d6fe75
panu0 <panu.linnal4@gmail.com>
2018-08-31 22:30:41 +0300

    Clean up boilerplate

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,5 +3,5 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+//CMS.registerPreviewTemplate('about', AboutPagePreview)
+//CMS.registerPreviewTemplate('blog', BlogPostPreview)
