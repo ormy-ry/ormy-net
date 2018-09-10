9af7342af74d627183bf44ecd425b4990ad9ddcf
panu0 <panu.linnal4@gmail.com>
2018-09-10 23:21:29 +0300

    Add burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="container">
-      <div className="navbar-menu is-active">
-        <div className="navbar-start">
-          <Link className="navbar-item is-uppercase" exact to="/">
-            etusivu
-          </Link>
-          <Link className="navbar-item is-uppercase" to="/event">
-            tapahtumat
-          </Link>
-          <Link className="navbar-item is-uppercase" to="/info">
-            info
-          </Link>
+export default class Navbar extends Component {
+  state = {
+    isActive: false,
+  }
+
+  toggleNav = () => {
+    console.log('ayy')
+    this.setState(prevState => ({
+      isActive: !prevState.isActive,
+    }))
+  }
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <a
+              className={
+                this.state.isActive
+                  ? 'navbar-burger is-active'
+                  : 'navbar-burger'
+              }
+              aria-label="menu"
+              aria-expanded="false"
+              onClick={this.toggleNav}
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+          <div
+            className={
+              ' navbar-menu' + (this.state.isActive ? ' is-active' : '')
+            }
+          >
+            <div className="navbar-start">
+              <Link className="navbar-item is-uppercase" exact to="/">
+                etusivu
+              </Link>
+              <Link className="navbar-item is-uppercase" to="/event">
+                tapahtumat
+              </Link>
+              <Link className="navbar-item is-uppercase" to="/info">
+                info
+              </Link>
+            </div>
+            <div className="navbar-end">
+              <a
+                href="https://goo.gl/forms/LvRdfpfqYieirYvx1"
+                className="navbar-item is-uppercase"
+              >
+                <FontAwesomeIcon
+                  style={{ marginRight: '0.3rem' }}
+                  icon="envelope"
+                />{' '}
+                sähköpostilista
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="navbar-end" />
-      </div>
-    </div>
-  </nav>
-)
-
-export default Navbar
+      </nav>
+    )
+  }
+}
