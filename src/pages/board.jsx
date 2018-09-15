899d5cff3c24bb651db5aee39a5995c42e3aaaf7
wkpsfbx <panu@protonmail.com>
2018-09-15 11:00:03 +0000

    Add board page

---
+++ src/pages/board.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Card from '../components/Card'
+import Content, { HTMLContent } from '../components/Content'
+
+export default class EventsPage extends React.Component {
+  render() {
+    const { data } = this.props
+    let boardArr = []
+    data.Board.edges.forEach((edge, i) =>
+      boardArr.push(
+        <Card className="tile is-child">
+          <div className="columns">
+            <div className="column">
+              <h3 className="title">
+                {data.Board.edges[i].node.frontmatter.title}
+                </h3>
+                <HTMLContent content={data.Board.edges[i].node.html} />
+            </div>
+              <div className="column">
+                <BoardPic
+                  src={data.Board.edges[i].node.frontmatter.picture}
+                />
+            </div>
+          </div>
+        </Card>
+      )
+    )
+    return (
+      <div
+        className="container"
+        style={{ paddingTop: '2rem', paddingBottom: '10rem' }}
+      >
+        <div className="columns">
+          <div className="column">
+            {boardArr}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+BoardsPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export const pageQuery = graphql`
+  query BoardsQuery {
+    Board: allMarkdownRemark(
+      limit: 10
+      filter: { frontmatter: { identifier: { eq: "board" } } }
+      sort: { fields: [frontmatter___year], order: DESC }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            picture
+            year
+          }
+        }
+      }
+    }
+  }
+`
